86daf368498c1fd53361f16b83b04725b8100a3e
¨Martina¨ <maalfie@itba.edu.ar>
2025-08-12 13:19:03 -0300

    ACTUALIZO HASTA EJERCICIO 5

--- clase1.js
+++ clase1.js
@@ -4,7 +4,8 @@ let pais = "Argentina";
 let diaNacimiento = "18/04/06"
 //cambio 2 
 console.log(ciudad,pais,diaNacimiento);
-console.log("Hola, mi nombre es Martin y soy de " + ciudad + ", " + pais + ". Nací el " + diaNacimiento + ".");
+console.log("Hola, mi nombre es Martina y soy de " + ciudad + ", " + pais + ". Nací el " + diaNacimiento + ".");
+
 //EJERCICIO 2
 let num1 = 34;
 let num2 = 39;
@@ -18,11 +19,44 @@ console.log(prom,div)
 
 //EJERCICIO 3
 let AUTO = {
-marca: "Audi",
-modelo: "Q3",
-año: 2023,
-}
+    marca: "Audi",
+    modelo: "Q3",
+    año: 2023,
+};
 
 console.log(typeof(AUTO.marca))
 console.log(typeof(AUTO.modelo))
 console.log(typeof(AUTO.año))
+
+//EJERCICIO 4
+const CIUDADES = [
+    "Sydney",
+    " Londres",
+    " Tokio",
+    " Nueva York",
+    " Madrid",
+];
+
+CIUDADES[2] = " Paris";
+console.log(CIUDADES + ".");
+
+//EJERCICIO 5
+let PELI1 = {
+    título: "Barbie",
+    director: "Greta Gerwig",
+    año: 2023,
+};
+
+let PELI2 = {
+    título: "Lilo y Stitch",
+    director: "Dean Fleischer Camp",
+    año: 2025,
+};
+
+let PELI3 = {
+    título: "Con Todos Menos Contigo",
+    director: "Will Gluck",
+};
+
+const PELICULAS = [PELI1, PELI2, PELI3];
+console.log(PELICULAS[2].director)
