01216c920279e902281c62a15d8f0c38ef41c117
¨Martina¨ <maalfie@itba.edu.ar>
2025-08-11 21:32:24 -0300

    commit inicial

---
+++ clase1.js
@@ -0,0 +1,28 @@
+//EJERCICIO 1
+let ciudad = "Buenos Aires";
+let pais = "Argentina";
+let diaNacimiento = "18/04/06"
+
+console.log(ciudad,pais,diaNacimiento);
+
+//EJERCICIO 2
+let num1 = 34;
+let num2 = 39;
+let num3 = 58;
+
+suma = (num1 + num2 + num3)
+prom = (suma/3)
+div = suma%2
+
+console.log(prom,div)
+
+//EJERCICIO 3
+let AUTO = {
+marca: "Audi",
+modelo: "Q3",
+año: 2023,
+}
+
+console.log(typeof(AUTO.marca))
+console.log(typeof(AUTO.modelo))
+console.log(typeof(AUTO.año))
